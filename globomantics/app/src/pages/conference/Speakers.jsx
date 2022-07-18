2aaecf004a51a06abdea209ec191a0e9eb4436d0
Abhishek Chandarana <44428844+whoisabbie@users.noreply.github.com>
2022-07-18 11:15:37 +0530

    Completed queries, mutations, updating data in client cache

--- globomantics/app/src/pages/conference/Speakers.jsx
+++ globomantics/app/src/pages/conference/Speakers.jsx
@@ -1,71 +1,137 @@
 import * as React from "react";
 import "./style-sessions.css";
+import { useParams } from 'react-router-dom';
+import { gql, useQuery, useMutation } from '@apollo/client';
 
-/* ---> Define queries, mutations and fragments here */
+const SPEAKER_ATTRIBUTES = gql`
+  fragment SpeakerInfo on Speaker {
+    id
+    name
+    bio
+    featured
+    sessions {
+      id
+      title
+    }
+  }
+`;
+
+const SPEAKERS = gql`
+  query speakers {
+    speakers {
+      ...SpeakerInfo
+    }
+  }
+  ${SPEAKER_ATTRIBUTES}
+`;
+
+const SPEAKER_BY_ID = gql`
+  query speakerById($id: ID!) {
+    speakerById(id: $id) {
+      ...SpeakerInfo
+    }
+  }
+  ${SPEAKER_ATTRIBUTES}
+`;
+
+const FEATURED_SPEAKER = gql`
+  mutation marFeatured($speakerId: ID!, $featured: Boolean!) {
+    markFeatured(speakerId: $speakerId, featured: $featured) {
+      id
+      featured
+    }
+  }
+`;
 
 const SpeakerList = () => {
 
-  /* ---> Replace hardcoded speaker values with data that you get back from GraphQL server here */
-  const featured = false;
+  const { loading, error, data } = useQuery(SPEAKERS);
 
-  return (
-		<div
-      key={'id'}
+  const [markFeatured] = useMutation(FEATURED_SPEAKER);
+
+  if (loading) return <p>Loading Speakers...</p>
+  if (error) return <p>Error loading speakers!</p>
+
+  return data.speakers.map(({ id, name, bio, sessions, featured }) => (
+    <div
+      key={id}
       className="col-xs-12 col-sm-6 col-md-6"
       style={{ padding: 5 }}
     >
       <div className="panel panel-default">
         <div className="panel-heading">
-          <h3 className="panel-title">{'Speaker: '}</h3>
+          <h3 className="panel-title">{'Speaker: ' + name}</h3>
         </div>
         <div className="panel-body">
-          <h5>{'Bio: ' }</h5>
+          <h5>{'Bio: ' + bio}</h5>
         </div>
         <div className="panel-footer">
           <h4>Sessions</h4>
-					{
-						/* ---> Loop through speaker's sessions here */
-					}
-          <span>	
-            <button	
-              type="button"	
-              className="btn btn-default btn-lg"	
-              onClick={()=> {
-                /* ---> Call useMutation's mutate function to mark speaker as featured */
-              }}	
-              >	
-                <i	
-                  className={`fa ${featured ? "fa-star" : "fa-star-o"}`}	
-                  aria-hidden="true"	
-                  style={{	
-                    color: featured ? "gold" : undefined,	
-                  }}	
-                ></i>{" "}	
-                Featured Speaker	
-            </button>	
-          </span>
+          {
+            sessions.map((session) => (
+              <span key={session.id}>
+                <button
+                  type="button"
+                  className="btn btn-default btn-lg"
+                  onClick={async () => {
+                    await markFeatured({
+                      variables: {
+                        speakerId: id, featured: true
+                      }
+                    })
+                  }}
+                >
+                  <i
+                    className={`fa ${featured ? "fa-star" : "fa-star-o"}`}
+                    aria-hidden="true"
+                    style={{
+                      color: featured ? "gold" : undefined,
+                    }}
+                  ></i>{" "}
+                  {session.title}
+                </button>
+              </span>
+            ))
+          }
         </div>
       </div>
     </div>
-	);
+  ));
 };
 
 const SpeakerDetails = () => {
 
-    /* ---> Replace hardcoded speaker values with data that you get back from GraphQL server here */
+  const { speaker_id } = useParams();
+
+  const { loading, error, data } = useQuery(SPEAKER_BY_ID, {
+    variables: {
+      id: speaker_id
+    }
+  });
+
+  if (loading) return <p>Loading speaker...</p>;
+  if (error) return <p>Error loading speaker!</p>;
+
+  const speaker = data.speakerById;
+  const { id, name, bio, sessions } = speaker;
+
   return (
-    <div key={'id'} className="col-xs-12" style={{ padding: 5 }}>
+    <div key={id} className="col-xs-12" style={{ padding: 5 }}>
       <div className="panel panel-default">
         <div className="panel-heading">
-          <h3 className="panel-title">{'name'}</h3>
+          <h3 className="panel-title">{name}</h3>
         </div>
         <div className="panel-body">
-          <h5>{'bio'}</h5>
+          <h5>{bio}</h5>
         </div>
         <div className="panel-footer">
-          {{
-						/* ---> Loop through speaker's sessions here */
-					}}
+          {
+            sessions.map(({ id, title }) => (
+              <span key={id} style={{ padding: 5 }}>
+                "{title}"
+              </span>
+            ))
+          }
         </div>
       </div>
     </div>
@@ -97,4 +163,4 @@ export function Speakers() {
   );
 }
 
-	
+
