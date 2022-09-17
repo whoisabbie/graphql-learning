11302e6eefd8a756b3a162fe9fe28d74ef279072
Abhishek Chandarana <44428844+whoisabbie@users.noreply.github.com>
2022-09-17 13:51:33 +0530

    Deleted back-end api dir, to be created from scratch

--- globomantics/api/resolvers/index.js
+++
@@ -1,8 +0,0 @@
-const Query = require("./query");
-const Mutation = require("./mutation");
-const Session = require("./sessions");
-const Speaker = require("./speakers");
-const User = require("./users");
-const resolvers = { Query, Mutation, Session, Speaker, User };
-
-module.exports = resolvers;
